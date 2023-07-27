aed869a514be4d1bb98aa1505be607f7964a13d1
dongRogen <3601778801@qq.com>
2023-07-27 20:41:43 +0800

    pref: split modules

---
+++ src/mark.rs
@@ -0,0 +1,119 @@
+/// 标记-猜-安全
+const M_GUESS_SAFE: u8 = 0x10;
+/// 标记-猜-可疑
+const M_GUESS_SUSP: u8 = 0x20;
+/// 标记-猜-确认地雷
+const M_GUESS_MINE: u8 = 0x30;
+
+#[derive(PartialEq, Eq)]
+pub enum MineGuess {
+    Mine,
+    Safe,
+    Suspicious,
+}
+
+/// 标记-插旗
+const M_FLAGGED: u8 = 0x40;
+/// 标记-打开
+const M_OPENED: u8 = 0x80;
+
+/// 状态
+/// 每一位的含义：
+/// ``` txt
+///      ↓↓↓↓-warn-附近地雷数（8<?<16为地雷）
+/// 0000 0000
+/// |||└ 未知/安全(0/1) 0x30 = 地雷
+/// ||└ 未知/可疑(0/1)  0x30 = 地雷
+/// |└ 是否标记(0/1)
+/// └ 是否打开(0/1)
+/// ```
+#[derive(Clone, Copy, Eq)]
+pub struct Mark(pub u8);
+impl Mark {
+    /// 根据选项构造一个标记
+    pub fn from(around: u8, guess: MineGuess, flagged: bool, opened: bool) -> Self {
+        let mut v = (around | 0xf0) ^ 0xf0;
+        if opened {
+            v |= M_OPENED;
+        }
+        if flagged {
+            v |= M_FLAGGED;
+        }
+        use MineGuess::*;
+        v |= match guess {
+            Mine => M_GUESS_MINE,
+            Safe => M_GUESS_SAFE,
+            Suspicious => M_GUESS_SUSP,
+        };
+        Self(v)
+    }
+    pub fn set_flag(&mut self) {
+        self.0 |= M_FLAGGED
+    }
+    pub fn is_flagged(self) -> bool {
+        (self.0 & M_FLAGGED) > 0
+    }
+
+    pub fn set_open(&mut self) {
+        self.0 |= M_OPENED
+    }
+    pub fn is_open(self) -> bool {
+        self.0 >= M_OPENED
+    }
+
+    pub fn set_mine(&mut self) {
+        self.0 = ((self.0 | 0xf) ^ 0xf) | 9
+    }
+    pub fn is_mine(self) -> bool {
+        ((self.0 | 0xf0) ^ 0xf0) > 8
+    }
+
+    pub fn set_safe(&mut self) {
+        if !self.is_mine() {
+            self.0 |= M_GUESS_SAFE;
+        }
+    }
+    pub fn set_suspicious(&mut self) {
+        self.0 |= M_GUESS_SUSP
+    }
+
+    pub fn is_guess(self) -> bool {
+        (self.0 & M_GUESS_MINE) > 0
+    }
+    pub fn guess_mine(self) -> bool {
+        (self.0 & M_GUESS_MINE) == M_GUESS_MINE
+    }
+    pub fn guess_safe(self) -> bool {
+        (self.0 & M_GUESS_MINE) == M_GUESS_SAFE
+    }
+    pub fn guess_suspicious(self) -> bool {
+        (self.0 & M_GUESS_MINE) == M_GUESS_SUSP
+    }
+
+    pub fn set_warn(&mut self, warn: u8) {
+        let mut w = warn;
+        if w > 15 {
+            w = (w | 0xf0) ^ 0xf0;
+        }
+        self.0 = ((self.0 | 0xf) ^ 0xf) | w
+    }
+    pub fn bump_warn(&mut self, add: bool) {
+        let w = self.get_warn();
+        if add {
+            if w < 15 {
+                self.0 += 1
+            }
+        } else if w > 0 {
+            self.0 -= 1;
+        }
+    }
+    #[inline]
+    pub fn get_warn(self) -> u8 {
+        (self.0 | 0xf0) ^ 0xf0
+    }
+}
+impl PartialEq for Mark {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0 | 0xf0) == (other.0 | 0xf0)
+    }
+}
---
+++ src/position.rs
@@ -0,0 +1,67 @@
+use std::fmt::Display;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Position(pub u8, pub u8);
+impl Position {
+    #[inline]
+    fn limit(v: u8, max: u8) -> std::ops::RangeInclusive<u8> {
+        let mut mx = v + 1;
+        let mut mi = 0;
+        if v > 0 {
+            mi = v - 1;
+        } else if v == (max - 1) {
+            mx = v;
+        }
+        mi..=mx
+    }
+    /// 获取周围一周的坐标（距离1单位）
+    /// ## Arguments
+    /// x 和 y 的最大可用值
+    /// ## Returns
+    /// 最少3个，最多8个有效坐标
+    pub fn get_around(self, Self(mx, my): Self) -> [Option<Position>; 8] {
+        let Self(x, y) = self;
+        let mut ls = [None; 8];
+        let mut t = 0;
+        for ty in Self::limit(y, my) {
+            for tx in Self::limit(x, mx) {
+                if tx == x && ty == y {
+                    continue;
+                }
+                ls[t] = Some(Self(tx, ty));
+                t += 1;
+            }
+        }
+        ls
+    }
+
+    /// 获取垂直和水平方向两端各延申1单位处的坐标（4个）
+    /// ## Arguments
+    /// x 和 y 的最大可用值
+    /// ## Returns
+    /// 最少2个，最多4个有效坐标
+    pub fn get_nearby(self, Self(mx, my): Self) -> [Option<Position>; 4] {
+        let Self(x, y) = self;
+        let mut ls = [None; 4];
+        let mut t = 0;
+        for ty in Self::limit(y, my) {
+            if ty != y {
+                ls[t] = Some(Self(x, ty));
+                t += 1;
+            }
+        }
+        for tx in Self::limit(x, mx) {
+            if tx != x {
+                ls[t] = Some(Self(tx, y));
+                t += 1;
+            }
+        }
+        ls
+    }
+}
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Position(x, y) = self;
+        write!(f, "(x:{x}, y:{y})")
+    }
+}
